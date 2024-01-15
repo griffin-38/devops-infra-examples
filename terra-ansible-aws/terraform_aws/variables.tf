8589f628014ce571002f0c17565383fcc8d8f115
griffin-38 <omar@miskinyar.info>
2024-01-14 21:32:59 -0800

    - adding ansible playbook - changes to readme structure - added additional variables

--- terra-ansible-aws/terraform_aws/variables.tf
+++ terra-ansible-aws/terraform_aws/variables.tf
@@ -1,4 +1,3 @@
-# You can define additional variables here
 variable "cluster_name" {
   description = "Name for the EKS cluster"
 }
@@ -11,6 +10,21 @@ variable "subnets" {
 variable "vpc_id" {
   description = "VPC ID where the EKS cluster will be deployed"
   type        = string
+}
+
+variable "public_key_path" {
+  description = "Path to the public key used for worker nodes"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to the private key used for worker nodes"
+  type        = string
+}
+
+variable "ssh_user" {
+  description = "SSH user name to connect to your instance."
+  default     = "omixalot"
 }
 
 # You can define additional variables here if needed
